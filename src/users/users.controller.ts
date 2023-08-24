0095235d2be57e215d283ce213517104762ce897
Ilija Vuk <ilijavuk14@gmail.com>
2023-08-24 12:25:23 +0200

    Replaced the nestjsx crud library with dataui's fork

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { Crud, CrudController } from '@nestjsx/crud';
+import { Controller } from '@nestjs/common';
+import { Crud, CrudController } from '@dataui/crud';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { TypeOrmCrudService } from '@dataui/crud-typeorm';
 
 @Injectable()
 export class UsersService extends TypeOrmCrudService<User> {
